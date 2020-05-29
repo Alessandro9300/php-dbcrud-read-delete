825a620a19655afbc2c5a07737e7e1388450c6ea
Alessandro9300 <aandreoli93@gmail.com>
2020-05-29 21:06:09 +0200

    chiamata al db

---
+++ api.php
@@ -0,0 +1,48 @@
+<?php
+
+  header('Content-Type: application/json');
+
+  $server = "localhost";
+  $username = "root";
+  $password = "root";
+  $dbName = "ex_2_db";
+
+  $conn = new mysqli($server, $username, $password, $dbName);
+
+  if ($conn -> connect_errno){
+    echo $conn -> connect_errno;
+    return;
+  }
+
+  $sql = "
+
+    SELECT id, name, lastname FROM paganti
+
+  ";
+
+  $results = $conn -> query($sql);
+
+  if ($results -> num_rows < 1){
+
+    echo "no result";
+    return;
+  }
+
+  $res = [];
+
+  while ($row = $results -> fetch_assoc()) {
+
+    $res[] =  [
+      "name" => $row["name"],
+      "lastname" => $row["lastname"],
+      "id" => $row["id"]
+    ];
+
+
+  }
+
+  $conn -> close();
+
+  echo json_encode($res);
+
+?>
